08f0c26b2e21757679a90c8a286715ba590ce115
Lucas Santos <lucas201273@gmail.com>
2025-04-13 11:07:32 -0300

    test: ensure OrderController should returns 400 if no clientName is provided

--- orderservice/app/src/test/java/br/com/lucassantos/orderservice/app/controller/OrderControllerTest.java
+++ orderservice/app/src/test/java/br/com/lucassantos/orderservice/app/controller/OrderControllerTest.java
@@ -1,7 +1,48 @@
 package br.com.lucassantos.orderservice.app.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.lucassantos.orderservice.app.dtos.request.OrderRequestDto;
+import br.com.lucassantos.orderservice.app.entity.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@ActiveProfiles("dev")
+@SpringBootTest
+@AutoConfigureMockMvc
 class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @DisplayName("POST- Should returns 400 if no clienteName is provided")
+    @Test
+    void postShouldReturnsBadRequestIfNoClientNameIsProvided() throws Exception{
+        Product product = new Product(UUID.randomUUID(), "any_name", 5, BigDecimal.valueOf(40.50), null);
+        OrderRequestDto orderRequestDto = new OrderRequestDto(null, List.of(product));
+        String jsonBody = objectMapper.writeValueAsString(orderRequestDto);
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(jsonBody));
+        resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
 
 }
